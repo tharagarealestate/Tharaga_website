1e1bfbccaa8b247eb4e8f5dbc16c46b14b216cbc
nithishrex-tharaga <nithishrex@gmail.com>
2025-12-14 05:58:45 +0530

    Fix buyer dashboard loading issue by adding SupabaseProvider and ReactQueryProvider to layout

--- app/app/(dashboard)/my-dashboard/layout.tsx
+++ app/app/(dashboard)/my-dashboard/layout.tsx
@@ -1,10 +1,14 @@
 "use client"
 import * as React from 'react'
+import { ReactQueryProvider } from '@/components/providers/ReactQueryProvider'
+import { SupabaseProvider } from '@/contexts/SupabaseContext'
 
 export default function MyDashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      {children}
-    </>
+    <ReactQueryProvider>
+      <SupabaseProvider>
+        {children}
+      </SupabaseProvider>
+    </ReactQueryProvider>
   )
 }

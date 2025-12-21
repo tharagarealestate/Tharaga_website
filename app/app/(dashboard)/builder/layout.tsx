27ad82786e372a0d5fbeb9a4a91eaf3963b061e2
nithishrex-tharaga <nithishrex@gmail.com>
2025-12-21 20:59:23 +0530

    Fix builder dashboard: Correct paddingTop to 60px for global header

--- app/app/(dashboard)/builder/layout.tsx
+++ app/app/(dashboard)/builder/layout.tsx
@@ -34,9 +34,9 @@ export default function BuilderDashboardLayout({ children }: { children: ReactNo
           style={{ 
             marginLeft: '220px',
             width: 'calc(100% - 220px)',
-            paddingTop: '72px',
+            paddingTop: '60px',
             marginTop: 0,
-            padding: '72px 0 0 0'
+            padding: '60px 0 0 0'
           }}
         >
           {children}
